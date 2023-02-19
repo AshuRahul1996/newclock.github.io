202fd879b7c12da90fe813c852d8ea8f1f3fc309
AshuRahul1996 <125209625+AshuRahul1996@users.noreply.github.com>
2023-02-19 18:15:49 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,18 @@
+console.log("this is clock.js");
+function updateClock(){
+    let currentTime = new Date();
+    let currentHour = currentTime.getHours();
+    let currentMinutes = currentTime.getMinutes();
+    let currentSeconds = currentTime.getSeconds();
+
+    currentMinutes = (currentMinutes <10 ? "0": "") + currentMinutes;
+    currentSeconds = (currentSeconds <10 ? "0" : "") + currentSeconds;
+    
+    let timeofDay = (currentHour<12)?"AM" :"PM";
+    currentHour = (currentHour>12)? currentHour-12 : currentHour;
+    currentHour = (currentHour==0) ?12 :currentHour;
+    let currentTimeStr = currentHour + ":" + currentMinutes + ":" + currentSeconds + "" + timeofDay;
+
+    document.getElementById("clock").innerHTML= currentTimeStr;
+
+}
